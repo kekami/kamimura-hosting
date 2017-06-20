a4ef76604d486a5b35b2a3c247abfec06d9cc32d
Kevin Kamimura <kevin.kamimura@outlook.com>
2017-06-21 00:04:46 +0200

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,36 @@
+function openOffer(evt, offerType) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(offerType).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen") ? document.getElementById("defaultOpen").click() : null;
+
+var modal = document.getElementById('loginModal');
+var btn = document.getElementById("openModal");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
